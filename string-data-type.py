bc309ba0703cb751352d4091fc381acd95bf02bc
EC2 Default User <ec2-user@ip-10-16-10-72.us-west-2.compute.internal>
2023-10-31 04:14:10 +0000

    ejercicio de string

---
+++ string-data-type.py
@@ -0,0 +1,19 @@
+# String str
+myString = "This is a string"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+# Cadenas de entrada
+name = input("what is your name? ")
+print(name)
+
+# Cadenas de salida
+color  = input("What is your favorite color? ")
+animal = input("What is your favorite animal? ")
+print("{}, your like a {} {}!".format(name, color, animal))

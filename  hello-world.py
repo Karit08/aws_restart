3671db331ecc678e553375644ccd8e0f5e740fa6
EC2 Default User <ec2-user@ip-10-16-10-72.us-west-2.compute.internal>
2023-11-01 23:58:48 +0000

    laboratorio de la secuencia de cadena y el peso de la insulina

---  hello-world.py
+++  hello-world.py
@@ -1,19 +1,6 @@
 print("Hello, world")
 
 
-myString = "This is a string"
-print(myString)
-print(type(myString))
-print(myString + " is of the data type " + str(type(myString)))
-firstString = "water"
-secondString = "fall"
-thirdString = firstString + secondString
-print(thirdString)
-# name = input("what is your name? ")
-# print(name)
-# color  = input("What is your favorite color? ")
-# animal = input("What is your favorite animal? ")
-# print("{}, your like a {} {}!".format(name, color, animal))
 myFruitList =["apple", "banana", "cherry"]
 print(myFruitList)
 print(type(myFruitList))

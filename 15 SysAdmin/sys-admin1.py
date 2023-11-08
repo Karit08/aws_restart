20066a513556c7897552dc5e22e1e076cb9cac55
EC2 Default User <ec2-user@ip-10-16-10-72.us-west-2.compute.internal>
2023-11-08 04:38:27 +0000

    modificaciones

--- 15 SysAdmin/sys-admin1.py
+++ 15 SysAdmin/sys-admin1.py
@@ -1,14 +1,14 @@
 import subprocess
 
-# subprocess.run(["ls"])
-# subprocess.run(["ls","-l"])
-# subprocess.run(["ls","-l","README.md"])
+subprocess.run(["ls"])
+subprocess.run(["ls","-l"])
+subprocess.run(["ls","-l","README.md"])
 
-# # Recupera información del sistema
-# command="uname"
-# commandArgument="-a"
-# print(f'Gathering system information with command: {command} {commandArgument}')
-# subprocess.run([command,commandArgument])
+# Recupera información del sistema
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
 
 # Recupera la indormación sobre el espacio en disco 
 command="ps"
